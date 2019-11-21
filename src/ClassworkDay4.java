5ffd1817db7ad35818b5fdefd807ace108bfd927
roman-cf <roman.springer@outlook.com>
2019-11-21 09:31:09 +0100

    A1 fertig

--- src/ClassworkDay4.java
+++ src/ClassworkDay4.java
@@ -1,5 +1,8 @@
 public class ClassworkDay4 {
     public static void main(String[] args) {
-        
+        // A1
+        for(int i = 1; i < 101; i++){
+            System.out.println(i);
+        }
     }
 }
