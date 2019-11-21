ed0d1ba8a1e76eb342af36c3e1c2ab9bb22e3a0f
Marian Kovacik <kovacik27@gmail.com>
2019-11-21 09:59:30 +0100

    A1 done

--- src/JavaDay4.java
+++ src/JavaDay4.java
@@ -1,2 +1,11 @@
 public class JavaDay4 {
-}
+    public static void main(String[] args) {
+        // A1 basic
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+
+
+        }
+    }
+    }
